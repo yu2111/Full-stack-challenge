b70a0622e0b194b030d8d6790c6bc2466a3798cd
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-04 01:15:39 +0700

    js practice

--- Capstone Projects/script.js
+++ Capstone Projects/script.js
@@ -16,3 +16,30 @@ function lifeInWeeks(age) {
 
 /*************Don't change the code below**********/
 }
+
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  var bottles = Math.floor(money / 1.5);
+  console.log("buy " + bottles + " bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+function bmiCalculator(weight, height){
+    return Math.round(weight/(height*height));
+}
