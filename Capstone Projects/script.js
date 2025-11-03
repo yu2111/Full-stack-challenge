9325aa9a669bfdd67b68525c1899e68e893001af
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-04 00:58:10 +0700

    js practice

--- Capstone Projects/script.js
+++ Capstone Projects/script.js
@@ -1,1 +1,18 @@
 console.log("test");
+
+function lifeInWeeks(age) {
+
+/************Don't change the code above************/
+
+    var yearLeft = 90 - age;
+    var monthLeft = yearLeft * 12;
+    var weekLeft = yearLeft * 52;
+    var dayLeft = yearLeft * 365;
+
+    console.log("You have " + dayLeft + " days, " + weekLeft + " weeks, and " + monthLeft + " months left.")
+
+
+
+
+/*************Don't change the code below**********/
+}
