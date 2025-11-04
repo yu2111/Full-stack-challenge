368b4323e84215794b0de74dbf81f8e25d05e5e6
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-04 22:39:35 +0700

    leapyear

--- Capstone Projects/script.js
+++ Capstone Projects/script.js
@@ -69,3 +69,12 @@ function bmiCalculator (weight, height) {
 }
 
 bmiCalculator(prompt("weight"),prompt("height"));
+
+function leapYear(year) {
+    if (year % 4 !== 0 || year % 4 === 0 && year % 100 === 0 && year % 400 !== 0){
+        return "not a leap year"
+    }
+    if (year % 4 === 0 && year % 100 !== 0 || year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+        return "a leap year"
+    }
+}
