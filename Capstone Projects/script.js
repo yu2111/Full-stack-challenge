b5cb4a056ed9a7b81d86bf85b7959acfd34b5ce9
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-04 22:08:53 +0700

    js

--- Capstone Projects/script.js
+++ Capstone Projects/script.js
@@ -53,3 +53,19 @@ function loveCal(name1, name2){
 }
 
 loveCal(name1, name2);
+
+function bmiCalculator (weight, height) {
+     var bmi = Math.round(weight/Math.pow(height, 2));
+    console.log(bmi);
+     if(bmi > 24.9) {
+         return "Your BMI is " + bmi + ", so you are overweight"
+     }
+     if(bmi < 24.9 && bmi > 18.5) {
+         return "Your BMI is " + bmi + ", so you have a normal weight"
+     }
+     else {
+         return "Your BMI is " + bmi + ", so you are underweight"
+     }
+}
+
+bmiCalculator(prompt("weight"),prompt("height"));
