cb1e51cd3b0670df717f0d1435bf29678a493338
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-04 23:50:07 +0700

    fizzbuzz

--- Capstone Projects/script.js
+++ Capstone Projects/script.js
@@ -96,3 +96,21 @@ function approval(name) {
 }
 
 approval(prompt("your name"));
+
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+    if (count % 5 === 0 && count % 3 === 0){
+        output.push("FizzBuzz")
+    } else if (count % 5 === 0){
+        output.push("Buzz")
+    } else if (count % 3 === 0){
+        output.push("Fizz")
+    } else {
+        output.push(count);
+    }
+    count++;
+
+    console.log(output);
+}
