1a123ffb4fcb6b0acf41f72b8e423cc63e1a372f
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-07 01:58:08 +0700

    dicee

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,1 +1,27 @@
 console.log("hello");
+
+function randomImg(img) {
+  var randomNumber = Math.ceil(Math.random()*6);
+  var source = "./images/dice"+randomNumber+".png";
+  document.querySelector(img).setAttribute("src",source);
+  document.querySelector(img).setAttribute("number",randomNumber);
+}
+
+randomImg(".img1");
+randomImg(".img2");
+
+var number1 = document.querySelector(".img1").getAttribute("number");
+var number2 = document.querySelector(".img2").getAttribute("number");
+
+
+if (number1 > number2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins!"
+  } else if (number1 < number2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins!"
+  } else {
+  document.querySelector("h1").innerHTML = "Draw"
+  }
+
+document.querySelector("button").addEventListener("click", function () {
+  window.location.reload();
+});
