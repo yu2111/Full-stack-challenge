d93634a6ad4a5060c6d8de7b98ae38f87bddb53f
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-11 01:17:05 +0700

    done

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -1,38 +1,72 @@
-const gamePattern = [];
-const userClicked = [];
-const level = 0;
+const colors = ["red", "blue", "yellow", "green"];
+let gamePattern = [];
+let userClicked = [];
+let level = 0;
+let started = false
 
 
 $(document).on("keyup", () => {
-  if (gamePattern.length === 0){
+  if (!started){
     nextSequence();
+    started = true
   }
 })
 
+function checkAnswer(currentLevel) {
+  if (userClicked[currentLevel] === gamePattern[currentLevel]) {
+    console.log("user "+userClicked[currentLevel]);
+    console.log("game "+gamePattern[currentLevel]);
+    if (userClicked.length === gamePattern.length) {
+      setTimeout(() => {
+      nextSequence()
+      }, 1000);
+    }
+  }
+  else{
+    $("body").addClass("game-over")
+    setTimeout(() => {
+      $("body").removeClass("game-over")
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    reset()
+  }
+}
+
 function nextSequence() {
+  userClicked = [];
+  level++;
   const randomNumber = Math.floor((Math.random() * 4));
-  const colors = ["red", "blue", "yellow", "green"];
   const chosenColor = colors[randomNumber];
   gamePattern.push(chosenColor);
   const lastColor = gamePattern[gamePattern.length - 1];
-  $("#"+lastColor).addClass("pressed");
-  const audio = new Audio("./sounds/"+lastColor+".mp3").play();
-  setTimeout(() => {
-     $("#"+lastColor).removeClass("pressed")
-  }, 300);
-  level++;
+  playSound(chosenColor);
+  animate(chosenColor);
   $("h1").text("Level "+level);
+  console.log(gamePattern);
 }
 
-function handler() {
   $(".btn").on("click", (e) => {
     const clicked = e.currentTarget.id;
     userClicked.push(clicked);
-    $("#"+clicked).addClass("pressed");
-    const audio = new Audio("./sounds/"+clicked+".mp3").play()
-    setTimeout(() => {
-    $("#"+clicked).removeClass("pressed")
-    }, 250);
-    console.log(userClicked);
+    playSound(clicked)
+    animate(clicked)
+    // console.log(userClicked);
+    checkAnswer(userClicked.length - 1);
   });
+
+function playSound(color) {
+  const audio = new Audio("./sounds/"+color+".mp3").play();
+}
+
+function animate(color) {
+   $("#"+color).addClass("pressed");
+  setTimeout(() => {
+     $("#"+color).removeClass("pressed")
+  }, 300);
+}
+
+function reset() {
+  started = false
+  gamePattern = []
+  level = 0
 }
