176bb1a0f3f355299a8e52909a4e4c9cd633b593
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-12 01:43:19 +0700

    npm

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+import {randomSuperhero} from "superheroes";
+var superName = randomSuperhero();
+
+console.log(`I'm ${superName}!`);
