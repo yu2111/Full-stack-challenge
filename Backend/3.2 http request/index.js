b413772f251ab002a6c379a651fb9701aabedebc
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-13 00:55:36 +0700

    req get

--- Backend/3.2 http request/index.js
+++ Backend/3.2 http request/index.js
@@ -5,3 +5,15 @@ const port = 3000;
 app.listen(port, ()=>{
   console.log("this is from server");
 })
+
+app.get("/", (req, res)=>{
+  res.send('<h1>Hello World</h1> <br> <a href= "/about">about</a> <br> <a href= "/contact">contact</a>');
+})
+
+app.get("/about", (req, res)=>{
+  res.send("<h1>I'm Yu Sekiguchi</h1>");
+})
+
+app.get("/contact", (req, res)=>{
+  res.send(`<a href= "https://linkedin.com">Linkedin</a>`);
+})
