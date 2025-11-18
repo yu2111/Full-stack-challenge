564d697aa1745ffda2f22f402a0830261c06bea1
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-19 00:54:12 +0700

    initial setup done

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,18 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res)=>{
+  res.sendFile(__dirname + "/public/index.html")
+});
+
+app.listen(port, ()=>{
+  console.log(`from port: ${port}`);
+});
