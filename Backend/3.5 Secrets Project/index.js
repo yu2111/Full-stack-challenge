9fa12c588f8defbae128e107b6342114a8976264
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-19 01:06:20 +0700

    secret password

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -2,6 +2,7 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 import express from "express";
+import bodyParser  from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -9,8 +10,22 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", (req, res)=>{
   res.sendFile(__dirname + "/public/index.html")
+});
+
+app.post("/check", (req, res)=>{
+  console.log(req.body.password);
+  const password = "ILoveCoding";
+  const userPw = req.body.password;
+  if (password === userPw){
+    res.sendFile(__dirname + "/public/secret.html")
+  }
+  else{
+    res.sendFile(__dirname + "/public/index.html")
+  }
 });
 
 app.listen(port, ()=>{
