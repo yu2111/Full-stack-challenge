69d1a1a9e71ab946fbdf86689708cc223aa39089
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-21 22:09:45 +0700

    band generator

--- Backend/4.4 Band Generator Project/index.js
+++ Backend/4.4 Band Generator Project/index.js
@@ -8,6 +8,7 @@ const port = 3000;
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
+app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
@@ -26,11 +28,18 @@ app.post("/submit", (req, res) => {
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
+  const randomAdj = adj[Math.floor(Math.random()*adj.length)]
+  const randomNoun = noun[Math.floor(Math.random()*adj.length)]
+  const bandName = `${randomAdj} ${randomNoun}`;
+  res.render("index.ejs", {
+    bandName: bandName,
+  })
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
 
 const adj = [
   "abandoned",
