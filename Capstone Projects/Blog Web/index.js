1ad30565ab52524cd0e4f03494b3bae5d5211fdb
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-24 23:52:12 +0700

    index.ejs

--- Capstone Projects/Blog Web/index.js
+++ Capstone Projects/Blog Web/index.js
@@ -5,3 +5,11 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res)=>{
+  res.render("index.ejs")
+})
+
+app.listen(port, (req, res)=>{
+  console.log(`from port: ${port}`);
+})
