09463fc018032ac2884f9cb1d7464887464d91cc
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-25 23:33:00 +0700

    need db

--- Capstone Projects/Blog Web/index.js
+++ Capstone Projects/Blog Web/index.js
@@ -3,13 +3,13 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-
+app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}));
 
 let blogs = [];
 
 app.get("/", (req, res)=>{
-  res.render("index.ejs")
+  res.render("index.ejs", blogs)
 })
 
 app.get("/write", (req, res)=>{
@@ -18,7 +18,8 @@ app.get("/write", (req, res)=>{
 
 app.post("/submit", (req, res)=>{
   blogs.unshift(req.body)
-  res.render("index.ejs",{blogs: blogs})
+  console.log(blogs);
+  res.redirect("/")
 })
 
 app.listen(port, (req, res)=>{
