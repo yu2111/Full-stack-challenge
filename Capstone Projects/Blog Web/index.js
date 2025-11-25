8d29ac99fed6c3897009d20dfcd35cbd0c43ba2d
Yu Sekiguchi <yu.sekiguchi2@gmail.com>
2025-11-25 23:09:48 +0700

    app post working

--- Capstone Projects/Blog Web/index.js
+++ Capstone Projects/Blog Web/index.js
@@ -6,8 +6,19 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+let blogs = [];
+
 app.get("/", (req, res)=>{
   res.render("index.ejs")
+})
+
+app.get("/write", (req, res)=>{
+  res.render("write.ejs")
+})
+
+app.post("/submit", (req, res)=>{
+  blogs.unshift(req.body)
+  res.render("index.ejs",{blogs: blogs})
 })
 
 app.listen(port, (req, res)=>{
